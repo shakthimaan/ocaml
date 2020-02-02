ffc6c50fde55147338cc374c385f24221dbbcda5
Shakthi Kannan <author@shakthimaan.com>
2020-02-02 10:39:37 +0530

    Use only camly_fatal_error

--- runtime/caml/misc.h
+++ runtime/caml/misc.h
@@ -199,9 +199,6 @@ CAMLextern void caml_fatal_error (char *, ...)
 #endif
 CAMLnoreturn_end;
 
-CAMLextern void caml_fatal_error_arg (const char *fmt,
-                                      const char *arg) Noreturn;
-
 #define Is_power_of_2(x) (((x) & ((x) - 1)) == 0)
 
 /* Detection of available C built-in functions, the Clang way. */
